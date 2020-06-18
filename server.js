5afc9cc7239698bc21395d71ecc3d11577df1d68
spencer-art <danbrownnie123@gmail.com>
2020-06-18 16:29:29 +0530

    hosted databse

--- server.js
+++ server.js
@@ -1,52 +1,53 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
-process.on('uncaughtException', err => {
-    console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
-    console.log(err.name, err.message);
-    process.exit(1);
+process.on("uncaughtException", (err) => {
+ console.log("UNCAUGHT EXCEPTION! 💥 Shutting down...");
+ console.log(err.name, err.message);
+ process.exit(1);
 });
 
 dotenv.config({
-    path: './config.env'
+ path: "./config.env",
 });
-const app = require('./app');
+const app = require("./app");
 
 // Connect to db
-mongoose.connect(process.env.DATABASE_LOCAL, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
-});
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function () {
-    console.log('Connected to MongoDB');
-});
+// mongoose.connect(process.env.DATABASE_LOCAL, {
+//  useNewUrlParser: true,
+//  useCreateIndex: true,
+//  useFindAndModify: false,
+//  useUnifiedTopology: true,
+// });
+// var db = mongoose.connection;
+// db.on("error", console.error.bind(console, "connection error:"));
+// db.once("open", function() {
+//  console.log("Connected to MongoDB");
+// });
 
-// const DB = process.env.DATABASE.replace(
-//     "<PASSWORD>",
-//     process.env.DATABASE_PASSWORD
-// );
+const DB = process.env.DATABASE.replace(
+ "<PASSWORD>",
+ process.env.DATABASE_PASSWORD
+);
 
-// mongoose
-//     .connect(DB, {
-//         useNewUrlParser: true,
-//         useCreateIndex: true,
-//         useFindAndModify: false
-//     })
-//     .then(() => console.log("DB connection successful!"));
+mongoose
+ .connect(DB, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+ })
+ .then(() => console.log("DB connection successful!"));
 
 const port = process.env.PORT || 8000;
 const server = app.listen(port, () => {
-    console.log(`App running on port ${port}...`);
+ console.log(`App running on port ${port}...`);
 });
 
-process.on('unhandledRejection', err => {
-    console.log('UNHANDLED REJECTION! 💥 Shutting down...');
-    console.log(err.name, err.message);
-    server.close(() => {
-        process.exit(1);
-    });
+process.on("unhandledRejection", (err) => {
+ console.log("UNHANDLED REJECTION! 💥 Shutting down...");
+ console.log(err.name, err.message);
+ server.close(() => {
+  process.exit(1);
+ });
 });
